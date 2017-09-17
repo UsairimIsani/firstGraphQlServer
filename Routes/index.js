7e36d5b93d3fd8fa1b395b48dbd91b92c1a25a2d
Usairim Isani <usairim.isani@outlook.com>
2017-09-16 22:08:22 -0700

    bug

--- Routes/index.js
+++ Routes/index.js
@@ -6,6 +6,6 @@ router.route('/getpatient').post(getPatient);
 router.route('/doctors').get(allDoctors);
 router.route('/getdoctor').post(getDoctor);
 router.route('/createpatient').post(createPatient);
-router.route('/deletepatient').post(deletepatient);
+router.route('/deletepatient').post(deletePatient);
 router.route('/createdoctor').post(createDoctor);
 export default router;
